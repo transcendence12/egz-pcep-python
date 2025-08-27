cb8bb182e4ea1cf8c4e9800f90d487ef12acab18
transcendence12 <gosiek.krawczuk@gmail.com>
2025-08-27 15:02:59 +0200

    add zad43

---
+++ zad43.py
@@ -0,0 +1,10 @@
+# the second assignement:
+
+vals = [0,1,2]
+vals[0], vals[1] = vals[1], vals[2]
+# Opcje:
+# extends the list
+# doesn't change the list's length
+# shortens the list
+
+# 👉 Odpowiedź: doesn't change the list's length
